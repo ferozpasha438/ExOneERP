9f989dc1c27bc38d3e7d7a22578e9f0ae0110ff5
emadmohsin0608 <ebm.mbm2002@gmail.com>
2024-08-19 15:11:31 +0530

    New Login Page Added.....

--- LS_ERP/LS.HRM/ClientApp/src/app/app-routing.module.ts
+++ LS_ERP/LS.HRM/ClientApp/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './account/login.component';
+import { Login2Component } from './account/Login/login2/login2.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
 
@@ -15,7 +16,9 @@ import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
-   { path: '', component: LoginComponent, pathMatch: 'full' },
+  { path: '', component: LoginComponent, pathMatch: 'full' },
+  // { path: '', component: Login2Component, pathMatch: 'full' },
+
   //{ path: 'login', component: LoginComponent },
   //{ path: 'home', component: HomeComponent },
   {
