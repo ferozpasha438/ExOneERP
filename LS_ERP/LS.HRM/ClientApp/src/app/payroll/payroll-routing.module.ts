564829afb0df63673481c17bc51269da37a4c40d
Narender Gorige <narender@logicsystems-me.com>
2024-08-28 18:31:22 +0530

    Payroll Process

--- LS_ERP/LS.HRM/ClientApp/src/app/payroll/payroll-routing.module.ts
+++ LS_ERP/LS.HRM/ClientApp/src/app/payroll/payroll-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { GetpayrollcomponentComponent } from './Setup/PayrollComponent/getpayrollcomponent/getpayrollcomponent.component';
 import { GetpayrollperiodComponent } from './Setup/PayrollPeriod/getpayrollperiod/getpayrollperiod.component';
 import { GetpayrollpackageComponent } from './Management/PayrollPackage/getpayrollpackage/getpayrollpackage.component';
+import { PayrollprocessComponent } from './Management/PayrollProcess/payrollprocess/payrollprocess.component';
 
 const routes: Routes = [
   { path: 'getpayrollcomponent', component: GetpayrollcomponentComponent },
   { path: 'getpayrollperiod', component: GetpayrollperiodComponent },
   { path: 'getpayrollpackage', component: GetpayrollpackageComponent },
+  { path: 'payrollprocess', component: PayrollprocessComponent },
 ];
 
 @NgModule({
